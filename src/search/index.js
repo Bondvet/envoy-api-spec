44126d4e4315c9d871eba66d07c1c0d214f6d6fe
Daniel Holzmann <d@velopment.at>
2023-03-15 16:19:56 +0100

    WIP

--- src/search/index.js
+++ src/search/index.js
@@ -10,10 +10,4 @@
  * @apiSuccess {Number} total
  * @apiUse Clients
  *
- * @apiSuccessExample {json} Success-Response:
- *     HTTP/1.1 200 OK
- *     {
- *         "firstname": "John",
- *         "lastname": "Doe"
- *     }
  */
---
+++ src/search/response.js
@@ -0,0 +1,14 @@
+/**
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *         "total": 4,
+ *         "clients": [
+ *             {
+ *                 "id": "asdf",
+ *                 "firstName": "Jane",
+ *                 "lastName": "Doe"
+ *             }
+ *         ]
+ *     }
+ */
