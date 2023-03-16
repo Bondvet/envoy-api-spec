b1be2213d825df46ccded14aae9f4f36a90d1210
Daniel Holzmann <d@velopment.at>
2023-03-16 10:18:36 +0100

    cleanup

--- src/search/response.js
+++ src/search/response.js
@@ -3,16 +3,4 @@
  * @apiSuccess {Number} total total number of clients found by
  *  the provided search parameters
  * @apiUse Clients
- * @apiSuccessExample {json} Success-Response:
- *     HTTP/1.1 200 OK
- *     {
- *         "total": 4,
- *         "clients": [
- *             {
- *                 "id": "asdf",
- *                 "firstName": "Jane",
- *                 "lastName": "Doe"
- *             }
- *         ]
- *     }
  */
