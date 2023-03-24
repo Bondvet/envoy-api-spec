d23e6e5bde1683226e21405f7357d14e07943e2c
Daniel Holzmann <d@velopment.at>
2023-03-24 13:57:37 +0100

    add patient age, weight, sex, and neutered

--- src/search/patient.js
+++ src/search/patient.js
@@ -4,5 +4,11 @@
  * @apiSuccess {String} clients.patients.id
  * @apiSuccess {String} [clients.patients.name]
  * @apiSuccess {String} [clients.patients.species]
+ * @apiSuccess {String} [clients.patients.age]
  * @apiSuccess {String} [clients.patients.breed]
+ * @apiSuccess {Object} [clients.patients.latestWeight]
+ * @apiSuccess {String = "G", "KG", "LB", "M2"} [clients.patients.latestWeight.unit]
+ * @apiSuccess {number} [clients.patients.latestWeight.value]
+ * @apiSuccess {Boolean} [clients.patients.neutered]
+ * @apiSuccess {String = "FEMALE", "MALE", "UNKNOWN"} [clients.patients.sex]
  */
