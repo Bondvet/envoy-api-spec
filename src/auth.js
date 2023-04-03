4c304ac5f424a2200cdf83fb179fe409c8bbf740
Daniel Holzmann <d@velopment.at>
2023-04-03 21:07:10 +0200

    change authorization header to Bearer

--- src/auth.js
+++ src/auth.js
@@ -1,5 +1,7 @@
 /**
  * @apiDefine Auth
- * @apiHeader {String} authorization API-key
+ * @apiHeader {String} authorization API-key, prefixed with `Bearer `.
+ * so f.e. if your API key is `MY_API_KEY`, the value of the `Authorization` header
+ * needs to be `Bearer MY_API_KEY`
  */
 
