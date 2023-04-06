650d95b5edbc7905be436faf9e529dfb52042873
Daniel Holzmann <d@velopment.at>
2023-04-06 18:23:19 +0200

    add note about intl phone number format

--- src/search/index.js
+++ src/search/index.js
@@ -4,7 +4,10 @@
  * @apiGroup Search
  *
  * @apiParam {String="phone","email"} type type of search
- * @apiQuery {String} value search value
+ * @apiQuery {String} value search value. in the case that `value` is a phone number,
+ * please make sure to properly format it in
+ * [E.164](https://support.zendesk.com/hc/en-us/articles/4408823756570-What-are-the-accepted-phone-number-formats-for-Talk-#:~:text=164%20format.-,E.,164%20format%20is%20%2B16175551212.)
+ * format
  * @apiUse Auth
  *
  * @apiUse ClientsResponse
